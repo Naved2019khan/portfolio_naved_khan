60b83d8998fc76894dbaab26c45b632664715d05
naved <naved2019khan@gmail.com>
2024-07-03 12:35:40 +0530

    new commit

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -8,7 +8,7 @@ const RecentProjects = () => {
   return (
     <div className='py-20' id="projects">
         <h1 className='heading'>
-            A small selection {' '}
+            A small selection{' '}
             <span className='text-purple'>recent projects</span>
         </h1>
         <div className='flex flex-wrap items-center gap-20 justify-center p-4 mt-10'>
